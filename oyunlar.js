c6a48b74a867e4ed1fd9250e2676d0b2fa9f5259
hggunay <hggunay@gmail.com>
2026-09-20 22:04:09 +0300

    Add files via upload

--- oyunlar.js
+++ oyunlar.js
@@ -32,7 +32,7 @@ function oyunKatalogu(){ return (typeof OYUN_LISTESI !== 'undefined') ? OYUN_LIS
 /* Oyun dosyaları iframe ile çekiliyor; index.html'deki `?s=` damgası onları
    kapsamıyor, dolayısıyla tarayıcı eski oyunu gösterebiliyor. Bir oyun
    dosyasını (oyunlar/*.html) her değiştirdiğinde bu tarihi de güncelle. */
-const OYUN_SURUM = '20260920c';
+const OYUN_SURUM = '20260920d';
 
 /* Hangi oyunlar açık? → id kümesi. Ziyarette ziyaret edilen kişiye bakar. */
 function acikOyunlar(kisi){
